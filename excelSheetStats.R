437609ee94b3ffe5270b1bd2abc55b43c52b3181
Emily Piche <e.p.piche@gmail.com>
2024-10-23 13:46:24 -0400

    updated code to get a better idea of what is in the other "processed data" sheets

--- excelSheetStats.R
+++ excelSheetStats.R
@@ -2,99 +2,147 @@
 ### epp
 ### 10.4.2024
 
-### Notes: this isn't a very efficient function but if you're only doing one
-### data download folder with ~10-15 files, it shouldn't be a problem
+### Notes: this isn't a very efficient function so i threw in a progress bar
+### so the user can have an estimate of how long it should take to process
 
-
-# if you've got the tidyverse library loaded, you may need to dump it before
-# running this function due to package conflicts
-detach("package:readxl", unload=TRUE)
-library(openxlsx)
 library(dplyr)
+library(readxl)
+library(progress)
 
 # set directory that contains sub directories of downloads
-forest_directory <- "C:/GitHub/TEROS_sensors/HubbardBrook/raw"
+forest_directory <- "C:/GitHub/TEROS_sensors/Coweeta/raw"
 
 # get file list
 file_list <- list.files(path = forest_directory,
-                        pattern = ".xlsx", 
-                        recursive = TRUE, 
+                        pattern = ".xlsx",
+                        recursive = TRUE,
                         full.names = TRUE)
 
 # initialize file stats data.frame for the loop
 file_stats <- data.frame(subdir = character(),
-                         fileName = character(), 
-                         sheetStatus = character(),
+                         fileName = character(),
                          sheetName = character(),
                          noRows = numeric(),
                          noCol = numeric(),
+                         dateMin = as.POSIXct(character()),
+                         dateMax = as.POSIXct(character()),
                          stringsAsFactors = FALSE)
 
-
-# function to that checks excel file for visible and hidden sheets
+# function to check excel file sheets
 check_sheets <- function(file) {
   # get values for table
   subdirectory <- dirname(file)
   subdirectory <- gsub(paste0(forest_directory, "/"), "", subdirectory)
   file_name <- basename(file)
-  wb <- loadWorkbook(file)
-  vis_stat <- sheetVisibility(wb)
-  sheet_names <- names(wb)
+  
+  # get all sheet names in the file
+  sheet_names <- excel_sheets(file)
+  
   # message output in console if 'Metadata' sheet missing
-  checkMeta <- sheet_names[vis_stat == "visible"]
-  if (!"Metadata" %in% checkMeta) {
-    message(paste("Warning: File", file_name, "in", subdirectory, 
-                  "does not contain a sheet called 'Metadata'"))}
-  # loop through all sheets in each file 
-  for (i in seq_along(sheet_names)) {
-    sheet_name <- sheet_names[i]
-    sheet_status <- vis_stat[i]
-    no_rows <- nrow(read.xlsx(file, sheet = sheet_name))
-    no_col <- ncol(read.xlsx(file, sheet = sheet_name))
-    # global attribution to rbind results from each file outside of function
+  if (!"Metadata" %in% sheet_names) {
+    message(paste("Warning: File", file_name, "in", subdirectory,
+                  "does not contain a sheet called 'Metadata'"))
+  }
+  
+  # loop through all sheets in each file
+  for (sheet_name in sheet_names) {
+    # Read the sheet data, skipping first 2 rows
+    sheet_data <- suppressMessages(read_excel(file, sheet = sheet_name, col_names = FALSE, skip = 3))
+    
+    # Get number of rows and columns
+    no_rows <- nrow(sheet_data)
+    no_col <- ncol(sheet_data)
+    if(sheet_name != "Metadata") {
+      date_min <- min(as.POSIXct(sheet_data$'...1'), na.rm = TRUE)
+      date_max <- max(as.POSIXct(sheet_data$'...1'), na.rm = TRUE)
+    } else {
+      date_min <- NA
+      date_max <- NA
+    }
+    
+    # Append results to the file_stats dataframe
     file_stats <<- rbind(file_stats, data.frame(subdir = subdirectory,
-                                               fileName = file_name,
-                                               sheetStatus = sheet_status,
-                                               sheetName = sheet_name,
-                                               noRows = no_rows,
-                                               noCol = no_col))}
+                                                fileName = file_name,
+                                                sheetName = sheet_name,
+                                                noRows = no_rows,
+                                                noCol = no_col,
+                                                dateMin = date_min,
+                                                dateMax = date_max))
+  }
 }
 
+
 # loop through all files in directory, check sheets in each file
-for (file in file_list) {check_sheets(file)}
+# also lets add a progress bar because i like knowing how long it will take
+pb <- progress_bar$new(
+  format = "Processing files [:bar] :percent :elapsed",
+  total = length(file_list), 
+  clear = FALSE, 
+  width = 60)
 
-# hidden vs visible sheets
-hidden_sheets <- file_stats %>% filter(sheetStatus == "hidden")
-visible_sheets <- file_stats %>% filter(sheetStatus == "visible")
+for (file in file_list) {
+  check_sheets(file)
+  #Progress bar to display file processing
+  pb$tick(tokens = list(file = file)) }
 
-# filter for files with more than one "processed data config" sheet
-visible_sheets_extra <- file_stats %>% filter(sheetStatus == "visible") %>% 
-  group_by(fileName) %>% 
-  filter(n() > 2) %>%
-  ungroup()
+# Are the metadata sheets all formatted the same?
+meta_cols <- file_stats %>% filter(sheetName == "Metadata") %>% select(-dateMin, -dateMax)
 
-# which files have extra sheets?
-extras <- visible_sheets_extra %>% group_by(fileName) %>% summarize(totalSheets=length(unique(sheetName)))
+# Filter for files with more than one "processed data config" sheet
+# filter out the Metadata sheet and the max processed data sheet 
+# (thats the one that we've automatically been using)
+# SO, this shows all the sheets that have been missed
+sheets_extra <- file_stats %>% 
+  distinct() %>%
+  filter(!grepl("Raw", sheetName)) %>%
+  filter(!grepl("Metadata", sheetName)) %>%
+  mutate(processedSheet = as.numeric(substr(sheetName, nchar(sheetName)-1, nchar(sheetName)))) %>%
+  select(subdir, fileName, processedSheet, sheetName, everything()) %>%
+  group_by(fileName) %>%
+  filter(n() > 1) %>%  
+  ungroup() 
 
-# are the metadata sheets all formatted the same?
-meta_cols <- file_stats %>% filter(sheetName == "Metadata")
+# these are all the sheets that have been missed!
+unprocessed_sheets <- sheets_extra %>% 
+  group_by(fileName) %>%
+  filter(processedSheet != max(processedSheet))
 
-# extract file names for files that contain extra sheets
-extra_files <- extras %>% pull(fileName)
+unprocessed_sheets <- unprocessed_sheets %>%
+  filter(noCol > 5)
 
-# what are the data that are in the extra sheets?
-file_list <- list.files(path = forest_directory,
-                        pattern = ".xlsx", recursive = TRUE) 
-file_list <- basename(file_list)
+# total date range for all the unprocessed sheets
+missed_sheets_dates <- unprocessed_sheets %>% ungroup() %>%
+  summarize(min = min(dateMin), max = max(dateMax))
 
-# for files in extra_files, what are the column names? headers?
-test1 <- read_excel(paste0(forest_directory,"/",file_list[16]), sheet = "Processed Data Configuration 1", col_names = FALSE, skip = 3)
-test2 <- read_excel(paste0(forest_directory,"/",file_list[16]), sheet = "Processed Data Configuration 2", col_names = FALSE, skip = 3)
-test3 <- read_excel(paste0(forest_directory,"/",file_list[16]), sheet = "Processed Data Configuration 3", col_names = FALSE, skip = 3)
 
-paste0(forest_directory,"/",file_list[16])
-difference <- setdiff(test1[,1], test2[,1])
 
-# range
-min(test1[,1])
-extra_files
+
+
+
+
+
+### ----------------------------------------------------------------------------
+# code playground below
+
+
+
+# what data are in these sheets??
+# make a for loop to read over all the column headers?
+
+for (i in 1:length(unprocessed_sheets$fileName)) {
+test <- suppressMessages(read_excel(paste0(forest_directory, "/", sheets_extra$subdir[i], "/", sheets_extra$fileName[i]), 
+                                    sheet = sheets_extra$sheetName[[i]], col_names = FALSE))
+
+}
+
+
+i <- 5
+test <- suppressMessages(read_excel(paste0(forest_directory, "/", sheets_extra$subdir[i], "/", sheets_extra$fileName[i]), 
+                                    sheet = sheets_extra$sheetName[[i]], col_names = FALSE)) 
+
+
+
+
+
+
+
